1d7d1a6cc1b8d0bb0439c967872f693813ebb24f
babylee2002 <babylee2002@gmail.com>
2018-10-11 08:08:21 -0400

    setup done models controllers created

---
+++ public/precache-manifest.c5d8a8d02a14bfeba29aa4f0dee12734.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9481fc8e7e92b97a7f3a",
+    "url": "/static/js/main.9481fc8e.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "9481fc8e7e92b97a7f3a",
+    "url": "/static/css/main.97509ae9.chunk.css"
+  },
+  {
+    "revision": "9941323650559391b9b317cd0e751996",
+    "url": "/index.html"
+  }
+];
